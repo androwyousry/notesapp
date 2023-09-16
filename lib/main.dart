7215b42f659317bba32224644cdf2d067523ad0a
androw yousry <androw.ymb@gmail.com>
2023-09-16 22:09:21 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Note());
+}
+
+class Note extends StatelessWidget {
+  const Note({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
